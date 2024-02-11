75a3e05298f5ffd5ddabfef42f10959e86c02c60
Ted <58144503+BUSH222@users.noreply.github.com>
2024-02-11 13:59:13 +0300

    Initial Commit

---
+++ Travel Statistics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel Statistics
+//
+//  Created by Ted Vtorov on 11.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
